f5da69ecddbdedc953e0a183f3c16a24c05f6961
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-09 21:38:01 +0800

    Added module for writing to a file

--- src/SymfonyHelperCommand.php
+++ src/SymfonyHelperCommand.php
@@ -46,4 +46,32 @@ class SymfonyHelperCommand extends Command
 
         return $file;
     }
+
+    /**
+     * Loops through each record in file and assigns to contents
+     * 
+     * @param  pointer $file 
+     * @return void
+     */
+    public function loop($file)
+    {
+        while(!feof($file)) {
+            $this->contents = fgets($file) . PHP_EOL;
+        }
+
+        fclose();
+    }
+
+    /**
+     * Write the contents to a file
+     * 
+     * @param  String $filename source file
+     * @param  String $message  message to write
+     * @return void
+     */
+    public function writeToFile($filename, $output, $message = '<comment>File created successfully</comment>')
+    {
+        file_put_contents($file, $this->contents);
+        $output->writeln($message);
+    }
 }
